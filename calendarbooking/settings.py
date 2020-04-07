4cd1d36989b415efb1a962251d8295ab2a80c182
thejaswinipathi <thejaswini_pathi@yahoo.co.in>
2020-04-07 14:28:15 +0530

    login page

--- calendarbooking/settings.py
+++ calendarbooking/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'booking'
 ]
 
 MIDDLEWARE = [
@@ -50,11 +51,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'calendarbooking.urls'
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- calendarbooking/urls.py
+++ calendarbooking/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+from booking import views as core_views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url(r'^signup/', core_views.signup, name='signup'),
+    url(r'^home/', admin.site.urls, name='home'),
+    url(r'^$', admin.site.urls, name='home'),
 ]
