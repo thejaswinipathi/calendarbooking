927a06bc6bee9f8dbd5abed50fef4e3579587cd5
Thejaswini Pathi <thejaswini_pathi@yahoo.co.in>
2020-04-10 18:58:55 +0530

    wsgi

--- calendarbooking/wsgi.py
+++ calendarbooking/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "calendarbooking.settings")
 
 application = get_wsgi_application()
 application = WhiteNoise(application)
