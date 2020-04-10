71a30c66b8ea52d9b5f0eb85972128233dc77b37
Thejaswini Pathi <thejaswini_pathi@yahoo.co.in>
2020-04-10 19:42:52 +0530

    home page

--- calendarbooking/settings.py
+++ calendarbooking/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG', default=False, cast=bool)
+DEBUG = False
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL')
